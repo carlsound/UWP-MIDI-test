6cfffdf9a8a3464c36481db51e0c29b6c3015d58
carlsound <johncarlson@johns-mbp.attlocal.net>
2017-11-26 13:34:07 -0600

    Added the Visual Studio project
    A Universal Windows Platform app that captures MIDI.

---
+++ W10 UWP CS MIDI Test 02/W10 UWP CS MIDI Test 02/MainPage.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Midi;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace W10_UWP_CS_MIDI_Test_02
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private DeviceInformationCollection midiInputDevicesCollection;
+        private DeviceInformationCollection midiOutputDevicesCollection;
+        //
+        private MidiEngine mdiEngine;
+        //
+        //
+        //
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        //
+        //
+        //
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            mdiEngine = new MidiEngine();
+            //
+            //
+            //
+            await PopulateMidiInputDeviceList();
+            await PopulateMidiOutputDeviceList();
+            //
+            //
+            //
+            mdiEngine.MidiInputMessageReceived += new EventHandler(this.UpdateMidiInputMessageListView);
+            mdiEngine.MidiOutputMessageSent += new EventHandler(this.UpdateMidiOutputMessageListView);
+        }
+        //
+        //
+        //
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            
+        }
+        //
+        //
+        //
+        private async Task PopulateMidiInputDeviceList()
+        {
+            MidiInputDevicesListBox.Items.Clear();
+            //
+            midiInputDevicesCollection = await DeviceInformation.FindAllAsync(MidiInPort.GetDeviceSelector());
+            //
+            foreach (var device in midiInputDevicesCollection)
+            {
+                MidiInputDevicesListBox.Items.Add(device.Name);
+            }
+            //
+            MidiInputDevicesListBox.SelectedIndex = 0;
+        }
+        //
+        //
+        //
+        private async Task PopulateMidiOutputDeviceList()
+        {
+            MidiOutputDevicesListBox.Items.Clear();
+            //
+            midiOutputDevicesCollection = await DeviceInformation.FindAllAsync(MidiOutPort.GetDeviceSelector());
+            //
+            foreach (var device in midiInputDevicesCollection)
+            {
+                MidiOutputDevicesListBox.Items.Add(device.Name);
+            }
+            //
+            MidiOutputDevicesListBox.SelectedIndex = 0;
+        }
+        //
+        //
+        //
+        private async void MidiInputDevicesListBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DeviceInformation di = midiInputDevicesCollection.ElementAt(MidiInputDevicesListBox.SelectedIndex);
+            await mdiEngine.setMidiInputPortId(di);
+        }
+        //
+        private async void MidiOutputDevicesListBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //this.mdiEngine.midiOutputPort = await MidiOutPort.FromIdAsync(midiOutputDevicesCollection[MidiOutputDevicesListBox.SelectedIndex].Id);
+            DeviceInformation di = midiInputDevicesCollection.ElementAt(MidiOutputDevicesListBox.SelectedIndex);
+            //await mdiEngine.setMidiOutputPortId(di);
+        }
+        //
+        //
+        //
+        private async void UpdateMidiInputMessageListView(object sender, EventArgs e)
+        {
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => 
+            {
+                MidiMessagesReceivedListView.Text += mdiEngine.midiMessagesReceived;
+                MidiMessagesReceivedListView.Text += "\n";
+            });
+           
+        }
+        //
+        //
+        //
+        private async void UpdateMidiOutputMessageListView(object sender, EventArgs e)
+        {
+            await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                MidiMessagesSentListView.Text += (Environment.NewLine + mdiEngine.midiMessagesSent);
+            });
+        }
+    }
+}
---
+++ W10 UWP CS MIDI Test 02/W10 UWP CS MIDI Test 02/MidiEngine.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Midi;
+using Windows.Storage.Streams;
+
+namespace W10_UWP_CS_MIDI_Test_02
+{
+    class MidiEngine//:Object
+    {
+        public MidiInPort midiInputPort { get; private set; }
+        public MidiOutPort midiOutputPort { get; private set; }
+        //
+        //
+        //
+        public String midiMessagesReceived { get; private set; }
+        public String midiMessagesSent { get; private set; }
+        //
+        //
+        //
+        public event EventHandler MidiInputMessageReceived;
+        public event EventHandler MidiOutputMessageSent;
+        //
+        //
+        //
+        public MidiEngine()
+        {
+            this.midiInputPort = null;
+            this.midiOutputPort = null;
+            this.midiMessagesReceived = "";
+            this.midiMessagesSent = "";
+        }
+        //
+        //
+        //
+        public async Task setMidiInputPortId(Windows.Devices.Enumeration.DeviceInformation Di)
+        {
+            this.midiInputPort = await MidiInPort.FromIdAsync(Di.Id);
+            //
+            this.midiInputPort.MessageReceived += MidiInputDevice_MessageReceived;
+        }
+        //
+        //
+        //
+        public async Task setMidiOutputPortId(Windows.Devices.Enumeration.DeviceInformation Di)
+        {
+            //this.midiOutputPort = await MidiOutPort.FromIdAsync(Di.Id);
+            //
+            //this.midiOutputPort.MessageReceived += MidiOutputDevice_MessageReceived;
+        }
+        //
+        //
+        //
+        private void MidiInputDevice_MessageReceived(MidiInPort sender, MidiMessageReceivedEventArgs args)
+        {
+            IMidiMessage receivedMidiMessage = args.Message;
+
+            // Build the received MIDI message into a readable format
+            StringBuilder outputMessage = new StringBuilder();
+            outputMessage.Append(receivedMidiMessage.Timestamp.ToString()).Append(", Type: ").Append(receivedMidiMessage.Type);
+
+            // Add MIDI message parameters to the output, depending on the type of message
+            switch (receivedMidiMessage.Type)
+            {
+                case MidiMessageType.NoteOff:
+                    var noteOffMessage = (MidiNoteOffMessage)receivedMidiMessage;
+                    outputMessage.Append(", Channel: ").Append(noteOffMessage.Channel).Append(", Note: ").Append(noteOffMessage.Note).Append(", Velocity: ").Append(noteOffMessage.Velocity);
+                    break;
+                case MidiMessageType.NoteOn:
+                    var noteOnMessage = (MidiNoteOnMessage)receivedMidiMessage;
+                    outputMessage.Append(", Channel: ").Append(noteOnMessage.Channel).Append(", Note: ").Append(noteOnMessage.Note).Append(", Velocity: ").Append(noteOnMessage.Velocity);
+                    break;
+                case MidiMessageType.PolyphonicKeyPressure:
+                    var polyphonicKeyPressureMessage = (MidiPolyphonicKeyPressureMessage)receivedMidiMessage;
+                    outputMessage.Append(", Channel: ").Append(polyphonicKeyPressureMessage.Channel).Append(", Note: ").Append(polyphonicKeyPressureMessage.Note).Append(", Pressure: ").Append(polyphonicKeyPressureMessage.Pressure);
+                    break;
+                case MidiMessageType.ControlChange:
+                    var controlChangeMessage = (MidiControlChangeMessage)receivedMidiMessage;
+                    outputMessage.Append(", Channel: ").Append(controlChangeMessage.Channel).Append(", Controller: ").Append(controlChangeMessage.Controller).Append(", Value: ").Append(controlChangeMessage.ControlValue);
+                    break;
+                case MidiMessageType.ProgramChange:
+                    var programChangeMessage = (MidiProgramChangeMessage)receivedMidiMessage;
+                    outputMessage.Append(", Channel: ").Append(programChangeMessage.Channel).Append(", Program: ").Append(programChangeMessage.Program);
+                    break;
+                case MidiMessageType.ChannelPressure:
+                    var channelPressureMessage = (MidiChannelPressureMessage)receivedMidiMessage;
+                    outputMessage.Append(", Channel: ").Append(channelPressureMessage.Channel).Append(", Pressure: ").Append(channelPressureMessage.Pressure);
+                    break;
+                case MidiMessageType.PitchBendChange:
+                    var pitchBendChangeMessage = (MidiPitchBendChangeMessage)receivedMidiMessage;
+                    outputMessage.Append(", Channel: ").Append(pitchBendChangeMessage.Channel).Append(", Bend: ").Append(pitchBendChangeMessage.Bend);
+                    break;
+                case MidiMessageType.SystemExclusive:
+                    var systemExclusiveMessage = (MidiSystemExclusiveMessage)receivedMidiMessage;
+                    outputMessage.Append(", ");
+
+                    // Read the SysEx bufffer
+                    var sysExDataReader = DataReader.FromBuffer(systemExclusiveMessage.RawData);
+                    while (sysExDataReader.UnconsumedBufferLength > 0)
+                    {
+                        byte byteRead = sysExDataReader.ReadByte();
+                        // Pad with leading zero if necessary
+                        outputMessage.Append(byteRead.ToString("X2")).Append(" ");
+                    }
+                    break;
+                case MidiMessageType.MidiTimeCode:
+                    var timeCodeMessage = (MidiTimeCodeMessage)receivedMidiMessage;
+                    outputMessage.Append(", FrameType: ").Append(timeCodeMessage.FrameType).Append(", Values: ").Append(timeCodeMessage.Values);
+                    break;
+                case MidiMessageType.SongPositionPointer:
+                    var songPositionPointerMessage = (MidiSongPositionPointerMessage)receivedMidiMessage;
+                    outputMessage.Append(", Beats: ").Append(songPositionPointerMessage.Beats);
+                    break;
+                case MidiMessageType.SongSelect:
+                    var songSelectMessage = (MidiSongSelectMessage)receivedMidiMessage;
+                    outputMessage.Append(", Song: ").Append(songSelectMessage.Song);
+                    break;
+                case MidiMessageType.TuneRequest:
+                    var tuneRequestMessage = (MidiTuneRequestMessage)receivedMidiMessage;
+                    //
+                    break;
+                case MidiMessageType.TimingClock:
+                    var timingClockMessage = (MidiTimingClockMessage)receivedMidiMessage;
+                    //
+                    break;
+                case MidiMessageType.Start:
+                    var startMessage = (MidiStartMessage)receivedMidiMessage;
+                    //
+                    break;
+                case MidiMessageType.Continue:
+                    var continueMessage = (MidiContinueMessage)receivedMidiMessage;
+                    //
+                    break;
+                case MidiMessageType.Stop:
+                    var stopMessage = (MidiStopMessage)receivedMidiMessage;
+                    //
+                    break;
+                case MidiMessageType.ActiveSensing:
+                    var activeSensingMessage = (MidiActiveSensingMessage)receivedMidiMessage;
+                    //
+                    break;
+                case MidiMessageType.SystemReset:
+                    var systemResetMessage = (MidiSystemResetMessage)receivedMidiMessage;
+                    //
+                    break;
+                case MidiMessageType.None:
+                    throw new InvalidOperationException();
+                default:
+                    break;
+            }
+            //
+            //
+            //
+            midiMessagesReceived = outputMessage.ToString();
+            //
+            EventArgs e = new EventArgs();
+            MidiInputMessageReceived(this, e);
+        }
+    }
+}
